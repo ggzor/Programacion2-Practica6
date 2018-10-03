34096add0eac98dbdcec0b5f4297ea1bdc6d6d58
ggzor <30713864+ggzor@users.noreply.github.com>
2018-10-02 21:41:45 -0500

    Agregando pruebas parametrizadas para contar oraciones

--- src/main/java/Analizador.java
+++ src/main/java/Analizador.java
@@ -4,29 +4,46 @@ public class Analizador {
 
   public Analizador(String entrada, String busqueda) {
     this.entrada = entrada;
-    this.busqueda = busqueda;    
+    this.busqueda = busqueda;
   }
 
   public int contarLineas() {
-    int contador = 0;
+    if (entrada.equals(""))
+      return 0;
 
+    int contador = 1;
     for (int i = 0; i < entrada.length(); i++) {
       if (entrada.charAt(i) == '\n') {
         contador++;
       }
     }
-    if (!entrada.equals("")) {
-      contador++;
-    }
-
     return contador;
   }
 
-  public String filtrarCaracteres() {
+/*   public String filtrarCaracteres() {
+    String nuevaCadena;
+    for (int i = 0; i < entrada.length; i++) {
+    }
+    if(entrada.charAt(i) >=<= )
     return entrada;
-  }
+  } */
 
   public int contarOraciones() {
-    return 0;
+    String valores[] = entrada.split("\\.");
+
+    int contador = 0;
+    for (int i = 0; i < valores.length; i++) {
+      if (valores[i].contains(" ")) {
+        if (i + 1 < valores.length) {
+          contador += 1;
+        } else {
+          if (entrada.endsWith(".")) {
+            contador += 1;
+          }
+        }
+      }
+    }
+
+    return contador;
   }
 }
--- src/test/java/AnalizadorTests.java
+++ src/test/java/AnalizadorTests.java
@@ -1,6 +1,9 @@
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class AnalizadorTests {
   private Analizador analizadorSoloEntrada(String entrada) {
@@ -27,37 +30,49 @@ public class AnalizadorTests {
 
     assertEquals(3, analizador.contarLineas());
   }
-
-  @Test
-  public void filtrarCaracteres_deberiaResgresar() {
-    Analizador analizador = analizadorSoloEntrada("CadenaA cadenaB\n cadenaC");
-
-    assertEquals("CadenaA cadenaB\n cadenaC", analizador.filtrarCaracteres());
-  }
-
-  @Test
-  public void filtrarCaracteres_deberiaResgresar() {
+  
+/*   @Test
+  public void filtrarCaracteres_deberiaRegresarCadenaVacia_cuandoCadenaVacia() {
     Analizador analizador = analizadorSoloEntrada("");
 
     assertEquals("", analizador.filtrarCaracteres());
   }  
   
   @Test
-  public void contarOraciones_deberiaRegresar0_cuandoCadenaVacia() {
-    Analizador analizador = analizadorSoloEntrada("");
+  public void filtrarCaracteres_deberiaRegresarLaMismaCadena_cuandoSoloTieneCaracteresValidos() {
+    Analizador analizador = analizadorSoloEntrada("CadenaA cadenaB\n cadenaC");
 
-    assertEquals(0, analizador.contarOraciones());
+    assertEquals("CadenaA cadenaB\n cadenaC", analizador.filtrarCaracteres());
   }
+  
 
-  @Test
-  public void contarOraciones_deberiaRegresar0_cuandoCadenaNoContienePuntos() {
-    Analizador analizador = analizadorSoloEntrada("Cadena");
 
-    assertEquals(0, analizador.contarOraciones());
-  }
+  @ParameterizedTest(name = "\"{1}\" debería filtrarse resultado en \"{0}\".")
+  @CsvSource(value={
+    "'',''", 
+   // "'',#$%",
+    "cadena,cadena",    
+  }) 
+  public void filtrarCaracteres_deberiaRegresar__cuandoCadenaEs__(String esperado, String cadena) {
+    Analizador analizador = analizadorSoloEntrada(cadena);
 
-  @Test
-  public void contarOraciones_deberiaRegresar0_cuandoCadenaContieneUnPunto_Y_LaCadenaNoContieneEspacios() {
-    Analizador analizador = analizadorSoloEntrada()
+    assertEquals(esperado, analizador.filtrarCaracteres());
+  }  */
+  
+  @ParameterizedTest(name="\"{1}\" debería tener {0} oraciones.")
+  @CsvSource(value={
+    "0,''",
+    "0,Cadena",
+    "0,Cadena.",
+    "0,.Cadena1",
+    "0,Cadena1.Cadena2.",
+    "1,Cadena1 .",
+    "1,Cadena1 Cadena2.",
+    "2,Cadena1 .Cadena2 ."
+  })
+  public void contarOraciones_deberiaRegresar__cuandoCadenaEs__(int esperado, String cadena) {
+    Analizador analizador = analizadorSoloEntrada(cadena);
+
+    assertEquals(esperado, analizador.contarOraciones());
   }
 }
